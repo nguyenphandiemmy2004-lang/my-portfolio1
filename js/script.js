10c3a954106341258c2e7830e8f5847e17bdec28
nguyenphandiemmy2004-lang <nguyenphandiemmy2004@gmail.com>
2026-07-17 07:47:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Nguyễn Phan Diễm My đã  load xong!');
 
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
